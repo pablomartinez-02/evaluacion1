6577ef5fe031cd3108e469a14b25914b66140007
pablomartinez-02 <55572515+pablomartinez-02@users.noreply.github.com>
2019-09-26 09:37:51 +0200

    Add files via upload

---
+++ saludador2.py
@@ -0,0 +1,131 @@
+Python 2.5.4 (r254:67916, Dec 23 2008, 15:19:34) [MSC v.1400 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+
+    ****************************************************************
+    Personal firewall software may warn about the connection IDLE
+    makes to its subprocess using this computer's internal loopback
+    interface.  This connection is not visible on any external
+    interface and no data is sent to or received from the Internet.
+    ****************************************************************
+    
+IDLE 1.2.4      
+>>> print "angel"
+angel
+>>> angel=1
+ç
+>>> angel=1
+>>> pablo=2
+>>> pablo+ angel
+3
+>>> pablo-angel
+1
+>>> angel-pablo
+-1
+>>> ================================ RESTART ================================
+>>> 
+>>> 
+>>> ================================ RESTART ================================
+>>> 
+>>> ================================ RESTART ================================
+>>> 
+>>> ================================ RESTART ================================
+>>> 
+>>> 
+>>> saludador_2
+<function saludador_2 at 0x0000000002B64208>
+>>> ================================ RESTART ================================
+>>> 
+hola amigo
+como te llamas?pablo
+
+Traceback (most recent call last):
+  File "C:/Users/inforbach1/Desktop/pablo martinez/saludador2.py", line 6, in <module>
+    saludador_2()
+  File "C:/Users/inforbach1/Desktop/pablo martinez/saludador2.py", line 3, in saludador_2
+    nombre=input("como te llamas?") #instruccion de entrada
+  File "<string>", line 1, in <module>
+NameError: name 'pablo' is not defined
+>>> ================================ RESTART ================================
+>>> 
+hola amigo
+
+Traceback (most recent call last):
+  File "C:/Users/inforbach1/Desktop/pablo martinez/saludador2.py", line 6, in <module>
+    saludador_2()
+  File "C:/Users/inforbach1/Desktop/pablo martinez/saludador2.py", line 3, in saludador_2
+    nombre=raw_imput("como te llamas?:") #instruccion de entrada
+NameError: global name 'raw_imput' is not defined
+>>> ================================ RESTART ================================
+>>> 
+hola amigo
+como te llamas?: pablo
+bienvenido a pythonpablo
+>>> ================================ RESTART ================================
+>>> 
+hola amigo
+como te llamas?: pablo
+bienvenido a pythonpablo
+>>> ================================ RESTART ================================
+>>> 
+hola amigo
+como te llamas?: angel
+bienvenido a pythonangel
+>>> ================================ RESTART ================================
+>>> 
+hola amigo
+como te llamas?: pablo
+bienvenido a pythonpablo
+cual es tu edad?:17
+
+Traceback (most recent call last):
+  File "C:/Users/inforbach1/Desktop/pablo martinez/saludador2.py", line 8, in <module>
+    saludador_2()
+  File "C:/Users/inforbach1/Desktop/pablo martinez/saludador2.py", line 6, in saludador_2
+    print "que mayor eres"+ edad
+TypeError: cannot concatenate 'str' and 'int' objects
+>>> ================================ RESTART ================================
+>>> 
+hola amigo
+como te llamas?: pablo
+bienvenido a pythonpablo
+cual es tu edad?:17
+que mayor eres17
+>>> ================================ RESTART ================================ç
+>>> 
+hola amigo
+como te llamas?: pablo
+bienvenido a pythonpablo
+cual es tu edad?:17
+que mayor eres17
+>>> ================================ RESTART ================================
+>>> 
+hola amigo
+como te llamas?: pablo
+bienvenido a pythonpablo
+cual es tu edad?:17
+que mayor eres17
+quien es tu jugador favorito:stephen curry
+el mio tambien esstephen curry
+>>> ================================ RESTART ================================
+>>> 
+hola amigo
+como te llamas?: pablo
+bienvenido a pythonpablo
+cual es tu edad?:17
+
+Traceback (most recent call last):
+  File "C:/Users/inforbach1/Desktop/pablo martinez/saludador2.py", line 11, in <module>
+    saludador_2()
+  File "C:/Users/inforbach1/Desktop/pablo martinez/saludador2.py", line 6, in saludador_2
+    print "que mayor eres"+ eda
+NameError: global name 'eda' is not defined
+>>> ================================ RESTART ================================
+>>> 
+hola amigo
+como te llamas?: pablo
+bienvenido a pythonpablo
+cual es tu edad?:17
+que mayor eres17
+quien es tu jugador favorito:stephen curry
+el mio tambien esstephen curry
+>>> 
