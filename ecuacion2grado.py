f1ec5937e89f86f40d1bbab18ae2533f286c731e
pablomartinez-02 <55572515+pablomartinez-02@users.noreply.github.com>
2019-09-27 09:33:04 +0200

    Add files via upload

--- ecuacion2grado.py
+++ ecuacion2grado.py
@@ -1,29 +1,18 @@
-def calcular(a, b, c):
-    import math
-    disc = (math.pow(b, 2)) - (4 * a * c)
-    if disc > 0:
-        x1 = (-b + math.sqrt(disc)) / (2 / a)
-        x2 = (-b - math.sqrt(disc)) / (2 / a)
-        print "\n\nLa ecacuacion tiene dos posibles soluciones: x1 = %.2f y x2 = %.2f" % (x1, x2)
-    else:
-        if disc == 0:
-            x1 = -b / (2 * a)
-            print "\n\nLa ecuacion tiene una posible solucion: x = %.2f" % (x1)
-        else:
-            xr = -b / (2 * a)
-            xi = (math.sqrt(-disc)) / (2 * a)
-            print "\n\nLa ecuacion tiene dos posibles soluciones, una real xr = %.2f y una imaginarinaria xi = %.2fi" % (xr, xi)
- 
-print "Solucionador de ecuaciones de segundo grado"
-print "_" * 43
-a = raw_input("\tEscriba el valor de a: ")
-while a == 0:
-    a = raw_input("\tEl valor de a no puede ser 0, por favor corrijalo: ")
-b = raw_input("\tEscriba el valor de b: ")
-c = raw_input("\tEscriba el valor de c: ")
- 
-a = float(a)
-b = float(b)
-c = float(c)
- 
-calcular(a, b, c)
+Python 2.5.4 (r254:67916, Dec 23 2008, 15:19:34) [MSC v.1400 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+
+    ****************************************************************
+    Personal firewall software may warn about the connection IDLE
+    makes to its subprocess using this computer's internal loopback
+    interface.  This connection is not visible on any external
+    interface and no data is sent to or received from the Internet.
+    ****************************************************************
+    
+IDLE 1.2.4      
+>>> ================================ RESTART ================================
+>>> 
+Valor de a: 4
+Valor de b: 23
+Valor de c: 0
+Soluciones de la ecuacion: x1=0.000 y x2=-5.750 
+>>> 
