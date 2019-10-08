2e136f0c2d2b39a5ccbb11c135ecc1364f0e58eb
pablomartinez-02 <55572515+pablomartinez-02@users.noreply.github.com>
2019-10-08 11:47:01 +0200

    Add files via upload

---
+++ ejemplofor.py
@@ -0,0 +1,7 @@
+def ejemplo_for():
+  #i es una variable que toma los valores comprendidos en el rango
+  final=input("hasta que numero: ")
+  for i in range(1,final+1):
+      print"i= ",i
+
+ejemplo_for()
