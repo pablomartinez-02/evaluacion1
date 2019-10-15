be6e7a953a4e4c1cf000615eae68f1a9312ec564
pablomartinez-02 <55572515+pablomartinez-02@users.noreply.github.com>
2019-10-15 11:58:11 +0200

    Add files via upload

---
+++ par e impar.py
@@ -0,0 +1,19 @@
+def numero():
+   num=input("numero ")
+   if num % 2 == 0:
+       print num," es par"
+   else:
+       print num, "es impar"
+   if num % 2 == 0:
+       print num,"multiplo de 2"
+   if num % 3 == 0:
+       print num,"multiplo de 3"
+   if num % 5 == 0:
+       print num,"multiplo de 5"
+   if num % 7 == 0:
+       print num,"multiplo de 7"
+   
+   
+       
+       
+numero()
