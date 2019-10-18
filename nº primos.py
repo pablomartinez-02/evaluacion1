0f55770f84c049e6d9f7b58647791ea73bb58304
pablomartinez-02 <55572515+pablomartinez-02@users.noreply.github.com>
2019-10-18 09:45:59 +0200

    Add files via upload

--- nº primos.py
+++ nº primos.py
@@ -1,12 +1,15 @@
 def primo():
     numero=input("say me your number")
     primo=True
-    for i in range(2,numero):
-     if (numero % i==0):
-         primo= False
-    if(primo==True):
-     print "es primo"
-    else:
-        print "no es primo"
+    for i in range(1,numero):
+        print i
+        for  cont in range(2,i):
+            if(i%cont==0):
+                primo=False
+        if( primo ==True):
+            print "es primo"
+        else:
+            print "no es primo"
+        primo=True
 primo()
 
