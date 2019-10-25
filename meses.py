e802ddfdac19ce72ea4a057a7aecae687a126ba5
pablomartinez-02 <55572515+pablomartinez-02@users.noreply.github.com>
2019-10-25 09:41:37 +0200

    Add files via upload

---
+++ meses.py
@@ -0,0 +1,28 @@
+def meses():
+    mes=input("introduzca un numero del 1 al 12: ")
+    if (mes==1):
+        print "es enero"
+    if (mes==2):
+        print"es febrero"
+    if (mes==3):
+        print"es marzo"
+    if (mes==4):
+        print"es abril"
+    if (mes==5):
+        print"es mayo"
+    if (mes==6):
+        print"es junio"
+    if (mes==7):
+        print"es julio"
+    if (mes==8):
+        print"es agosto"
+    if (mes==9):
+        print"es septiembre"
+    if (mes==10):
+        print"es octubre"
+    if (mes==11):
+        print"es noviembre"
+    if (mes==12):
+        print"es diciembre"
+        
+meses()
