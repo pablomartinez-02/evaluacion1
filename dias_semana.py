e4d90a384f4a620dc500a5415199031e57fafc18
pablomartinez-02 <55572515+pablomartinez-02@users.noreply.github.com>
2019-10-25 09:35:12 +0200

    Add files via upload

---
+++ dias_semana.py
@@ -0,0 +1,17 @@
+def dia_semana():
+    dia=input("digame un numero de la semana: ")
+    if (dia==1):
+      print"es lunes"
+    if (dia==2):
+      print"es martes"
+    if (dia==3):
+      print"es miercoles"
+    if (dia==4):
+      print"es jueves"
+    if (dia==5):
+      print"es viernes"
+    if (dia==6):
+      print"es sabado"
+    if (dia==7):
+      print"es domingo"
+dia_semana()
