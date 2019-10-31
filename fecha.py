0463a48e283b144f888ccf774e7609d75550488d
pablomartinez-02 <55572515+pablomartinez-02@users.noreply.github.com>
2019-10-31 09:32:45 +0100

    Add files via upload

---
+++ fecha.py
@@ -0,0 +1,30 @@
+def fecha():
+    dia=input("intrduzca un numero del 1 al 31: ")
+    mes=input("introduzca un numero del 1 al 12: ")
+    ano=input("introduzca el año")
+    print dia,
+    if (mes==1):
+        print "de enero",ano
+    if (mes==2):
+        print"de febrero",ano
+    if (mes==3):
+        print"de marzo",ano
+    if (mes==4):
+        print"de abril",ano
+    if (mes==5):
+        print"de mayo",ano
+    if (mes==6):
+        print"de junio",ano
+    if (mes==7):
+        print"de julio",ano
+    if (mes==8):
+        print"de agosto",ano
+    if (mes==9):
+        print"de septiembre",ano
+    if (mes==10):
+        print"de octubre",ano
+    if (mes==11):
+        print"de noviembre",ano
+    if (mes==12):
+        print"de diciembre",ano
+fecha()
