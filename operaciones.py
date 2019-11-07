3c0306346beb7d809c923bcb385d5a90ae41c582
pablomartinez-02 <55572515+pablomartinez-02@users.noreply.github.com>
2019-11-07 09:05:53 +0100

    Add files via upload

---
+++ operaciones.py
@@ -0,0 +1,13 @@
+def operacion():
+    a=input("introduce un numero: ")
+    b=input("introduce otro numero: ")
+    c=raw_input("que quieres hacer: ")
+    if(c=="S"):
+     print a+b
+    if(c=="R"):
+     print a-b
+    if(c=="M"):
+     print a*b
+    if(c=="D"):
+     print a/b
+operacion()
