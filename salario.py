d33a160f2561bac65e920b32d6031a1703301757
pablomartinez-02 <55572515+pablomartinez-02@users.noreply.github.com>
2019-11-19 11:30:34 +0100

    Add files via upload

---
+++ salario.py
@@ -0,0 +1,7 @@
+def salario():
+    horas_trabajadas=input("cuantas horas has trabajado: ")
+    horas_extra=input("cuantas horas extra has hecho: ")
+    print horas_trabajadas*30
+    print horas_extra*30*1.5
+    print horas_trabajadas*30+horas_extra*30*1.5
+salario()
